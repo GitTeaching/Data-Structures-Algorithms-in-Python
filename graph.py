8abae9b08c153828089fbdd2c73499abb6a89b43
Aicha Aid <aid_aicha@hotmail.fr>
2023-01-21 09:48:33 +0100

    Add files via upload

--- graph.py
+++ graph.py
@@ -24,6 +24,32 @@ class Graph:
         for vertex, neighbors in self.adjacent_list.items():
             #print(f"{vertex} --> {neighbors}")
             print(f"{vertex} --> {' '.join(neighbors)}")
+    
+    def bfs_traversal(self, source_node):
+        visited = [source_node]
+        queue = [source_node]
+        while queue:
+            curr = queue.pop(0)
+            for neighbor in self.adjacent_list[curr]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+                    visited.append(neighbor)
+        
+        return visited
+    
+    def dfs_traversal(self, source_node):
+        visited = []
+        stack = [source_node]
+        while stack:
+            curr = stack.pop()
+            if curr not in visited:
+                visited.append(curr)
+            for v in self.adjacent_list[curr]:
+                if v not in visited :
+                    stack.append(v)
+                    
+        return visited
+
 
 
 g = Graph()
@@ -36,16 +62,20 @@ g.add_vertex('4')
 g.add_vertex('5')
 g.add_vertex('6')
 
-g.add_edge('3', '1')
-g.add_edge('3', '4')
-g.add_edge('4', '2')
-g.add_edge('4', '5')
-g.add_edge('1', '2')
-g.add_edge('1', '0')
+g.add_edge('0', '1')
 g.add_edge('0', '2')
-g.add_edge('6', '5')
+g.add_edge('1', '2')
+g.add_edge('1', '3')
+g.add_edge('2', '4')
+g.add_edge('3', '4')
+g.add_edge('4', '5')
+g.add_edge('5', '6')
 
 g.show_connections()
+
+
+print(g.bfs_traversal('0'))
+print(g.dfs_traversal('0'))
 
 g.add_vertex('6')
 g.show_connections()
